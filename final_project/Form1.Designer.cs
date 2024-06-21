6c61f0f935e39252bc11b9004342bb1ee109c088
Nicpon99 <Nicpon044@onet.pl>
2024-06-21 14:50:54 +0200

    done

--- final_project/Form1.Designer.cs
+++ final_project/Form1.Designer.cs
@@ -28,20 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.textBoxDetails = new System.Windows.Forms.TextBox();
+            this.btnLoadUsers = new System.Windows.Forms.Button();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.btnDeleteUser = new System.Windows.Forms.Button();
+            this.btnSaveToFile = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.Location = new System.Drawing.Point(310, 329);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(173, 121);
+            this.listBoxUsers.TabIndex = 0;
+            this.listBoxUsers.SelectedIndexChanged += new System.EventHandler(this.listBoxUsers_SelectedIndexChanged);
+            // 
+            // textBoxDetails
+            // 
+            this.textBoxDetails.Location = new System.Drawing.Point(624, 329);
+            this.textBoxDetails.Multiline = true;
+            this.textBoxDetails.Name = "textBoxDetails";
+            this.textBoxDetails.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDetails.TabIndex = 1;
+            // 
+            // btnLoadUsers
+            // 
+            this.btnLoadUsers.Location = new System.Drawing.Point(310, 486);
+            this.btnLoadUsers.Name = "btnLoadUsers";
+            this.btnLoadUsers.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadUsers.TabIndex = 2;
+            this.btnLoadUsers.Text = "Load Users";
+            this.btnLoadUsers.UseVisualStyleBackColor = true;
+            this.btnLoadUsers.Click += new System.EventHandler(this.btnLoadUsers_Click);
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(553, 485);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.TabIndex = 7;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.Location = new System.Drawing.Point(686, 486);
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteUser.TabIndex = 8;
+            this.btnDeleteUser.Text = "Delete User";
+            this.btnDeleteUser.UseVisualStyleBackColor = true;
+            this.btnDeleteUser.Click += new System.EventHandler(this.btnDeleteUser_Click);
+            // 
+            // btnSaveToFile
+            // 
+            this.btnSaveToFile.Location = new System.Drawing.Point(823, 485);
+            this.btnSaveToFile.Name = "btnSaveToFile";
+            this.btnSaveToFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveToFile.TabIndex = 9;
+            this.btnSaveToFile.Text = "Save to File";
+            this.btnSaveToFile.UseVisualStyleBackColor = true;
+            this.btnSaveToFile.Click += new System.EventHandler(this.btnSaveToFile_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(553, 430);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 3;
+            this.textBoxName.Text = "Name";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(686, 430);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUsername.TabIndex = 6;
+            this.textBoxUsername.Text = "Username";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(823, 430);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmail.TabIndex = 5;
+            this.textBoxEmail.Text = "Email";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(953, 430);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPhone.TabIndex = 4;
+            this.textBoxPhone.Text = "Phone";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1428, 814);
+            this.ClientSize = new System.Drawing.Size(1360, 726);
+            this.Controls.Add(this.btnSaveToFile);
+            this.Controls.Add(this.btnDeleteUser);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.btnLoadUsers);
+            this.Controls.Add(this.textBoxDetails);
+            this.Controls.Add(this.listBoxUsers);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.TextBox textBoxDetails;
+        private System.Windows.Forms.Button btnLoadUsers;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Button btnDeleteUser;
+        private System.Windows.Forms.Button btnSaveToFile;
     }
 }
 
--- final_project/Form1.cs
+++ final_project/Form1.cs
@@ -7,14 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace final_project
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+        private JArray users = new JArray();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void btnLoadUsers_Click(object sender, EventArgs e)
+        {
+            string url = "https://jsonplaceholder.typicode.com/users";
+            var response = await client.GetStringAsync(url);
+            users = JArray.Parse(response);
+
+            listBoxUsers.Items.Clear();
+            foreach (var user in users)
+            {
+                listBoxUsers.Items.Add(user["name"]);
+            }
+        }
+
+        private void listBoxUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxUsers.SelectedIndex == -1 || users == null) return;
+
+            var selectedUser = users[listBoxUsers.SelectedIndex];
+
+            textBoxDetails.Text = $"Name: {selectedUser["name"]}\n" +
+                                  $"Username: {selectedUser["username"]}\n" +
+                                  $"Email: {selectedUser["email"]}\n" +
+                                  $"Phone: {selectedUser["phone"]}";
+        }
+
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            var newUser = new JObject
+            {
+                { "name", textBoxName.Text },
+                { "username", textBoxUsername.Text },
+                { "email", textBoxEmail.Text },
+                { "phone", textBoxPhone.Text }
+            };
+
+            users.Add(newUser);
+            listBoxUsers.Items.Add(newUser["name"]);
+            ClearInputFields();
+        }
+
+        private void btnDeleteUser_Click(object sender, EventArgs e)
+        {
+            if (listBoxUsers.SelectedIndex == -1) return;
+
+            users.RemoveAt(listBoxUsers.SelectedIndex);
+            listBoxUsers.Items.RemoveAt(listBoxUsers.SelectedIndex);
+            textBoxDetails.Clear();
+        }
+
+        private void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            var filePath = "users.json";
+            File.WriteAllText(filePath, users.ToString());
+            MessageBox.Show($"Users saved to {filePath}");
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxName.Clear();
+            textBoxUsername.Clear();
+            textBoxEmail.Clear();
+            textBoxPhone.Clear();
         }
     }
 }
